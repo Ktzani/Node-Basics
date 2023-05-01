e2167d753aab2b0d757c315c301e10beadf57f90
Augusto Ferreira Guilarducci <augustofgui@gmail.com>
2023-05-01 16:07:32 -0300

    corrige erros logicos nos testes

--- Formacao Node/Parte 29 - Projeto sistema de compartilhamento de imagens/test/user.test.js
+++ Formacao Node/Parte 29 - Projeto sistema de compartilhamento de imagens/test/user.test.js
@@ -4,6 +4,7 @@ const app = require('../src/app');
 let request = supertest(app)
 
 let mainUser = {name: 'John', email: 'john@example.com', password: 'password'}
+let unregisteredUser = {name: 'NotJohn', email: 'wrong@example.com', password: 'wrong'}
 
 //? Um Global afeta apenas a switch de teste que ele está inserido. CADA SWITCH É INDEPENDENTE UMA DA OUTRA
 //Função que vai ser executada antes de qualquer teste rodar e é como se fosse uma função global em que 
@@ -31,14 +32,14 @@ afterAll(() => {
 
 
 //Roda uma vez antes de cada teste dentro da minhas switch, ou seja, antes de cada teste ele é executado
-beforeEach(() => {
+// beforeEach(() => {
 
-}) 
+// }) 
 
 //Mesma logica do beforeEach. A diferença é que ele roda apos cada teste ao inves de antes de cada teste
-afterEach(() => {
+// afterEach(() => {
 
-}) 
+// }) 
 
 describe("Cadastro de usuario", () => {
     test("Deve cadastrar o usuário com sucesso", () => {
@@ -108,7 +109,7 @@ describe("Autenticação", () => {
 
     test("Deve impedir que um usuario não cadastrado se logue", () => {
         return request.post("/auth")
-            .send({email: mainUser.email, password: mainUser.password})
+            .send({email: unregisteredUser.email, password: unregisteredUser.password})
             .then(res => {
                 expect(res.statusCode).toEqual(404)
                 expect(res.body.error).toEqual("Usuário não encontrado") //Me garante que o token foi recebido (não é undefined)
@@ -120,7 +121,7 @@ describe("Autenticação", () => {
 
     test("Deve impedir que um usuario logue com a senha errada", () => {
         return request.post("/auth")
-            .send({email: mainUser.email, password: mainUser.password})
+            .send({email: mainUser.email, password: unregisteredUser.password})
             .then(res => {
                 expect(res.statusCode).toEqual(406)
                 expect(res.body.error).toEqual("Senha incorreta") //Me garante que o token foi recebido (não é undefined)
